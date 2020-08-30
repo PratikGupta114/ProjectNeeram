83ea18d5f0874b916fed614649042c1a12372671
pratik gupta <rgrgupta1148@gmail.com>
2020-08-30 10:22:46 +0530

    New Update to LineChartManager class.

--- app/src/main/java/com/example/waterlevelindicator/LineChartManager.java
+++ app/src/main/java/com/example/waterlevelindicator/LineChartManager.java
@@ -2,9 +2,20 @@ package com.example.waterlevelindicator;
 
 import android.app.Activity;
 
+import com.github.mikephil.charting.charts.LineChart;
+
 public class LineChartManager {
 
-    private MainActivity activity;
+    public static final int ACTIVITY_MAIN = 324234;
 
+    private MainActivity    activity;
+    private LineChart   lineChart;
+
+    public LineChartManager(Activity activity, int ActivityCode) {
+        if(ActivityCode == ACTIVITY_MAIN)
+            this.activity = (MainActivity ) activity;
+    }
+
+    // TODO - create a method to accept new data and display it on the graph.
 
 }
